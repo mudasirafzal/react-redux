43732b86fc31d37c379de0bec6b19ba527a9354a
mudasir-restoguru <mudasir@resto.guru>
2020-12-12 23:22:59 +0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Workouts from "./Components/Workouts";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Work Tracker</h1>
+      <Workouts />
+    </div>
+  );
+}
+
+export default App;
